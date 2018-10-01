a1d66bb94e907797e66425a3a4c83f836a45f195
jizhi <theniceangel@163.com>
2018-10-01 16:35:27 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  title: 'Vue Router源码分析',
+  base: '/learn-vue-router/',
+  head: [],
+  description: 'Vue Router 源码分析',
+  themeConfig: {
+    nav: [
+      { text: 'Github', link: 'https://github.com/theniceangel/learn-vue-router' }
+    ],
+    sidebar:[
+      {
+        title: '介绍',
+        collapsable: false,
+        children: [
+          '/introduce'
+        ]
+      }
+    ]
+  }
+}
